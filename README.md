a2be28128898e5893f3e2aea1beaf1801d7ded7c
artyuicraft <artyuicraft@gmail.com>
2024-02-12 16:58:59 +0100

    added code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-"# FuzionLaunch" 
+# FuzionLaunch
+
+FuzionLaunch is a launcher for apps entirely based around plugins. At its core, it's just a menu that loads menus/apps (it uses fzf for menus).
+
+## Installation
+
+1. Clone the repository.
+2. If you are on Windows, run the .bat file. If you are on Linux or macOS, run the .sh file.
+
+## Plugins
+
+Plugins can be installed in the `plugins` folder.
+
+### Plugin Creation
+
+To create a plugin, follow these steps:
+
+1. Import the plugin library:
+
+```python
+import plugins.pluglib as pluglib
+```
+2. Create the main function:
+```python
+def main():
+    return pluglib.menu()
+```
+The main function must return a pluglib.menu. The parameters for pluglib.menu are as follows:
+```python
+pluglib.menu(
+    name,  # Used for the name in the menu; must be a string
+    func   # The code that needs to be run when selected; must be callable
+)
+```
---
+++ main.py
@@ -0,0 +1,43 @@
+import fzfmenus
+import os
+import importlib.util
+
+main_menu = []
+
+def plugin_folder():
+    main_py_directory = os.path.dirname(os.path.abspath(__file__))
+    plugin_folder = os.path.join(main_py_directory, 'plugins')
+
+    return plugin_folder
+
+def discover_plugins(plugin_folder):
+    plugins = []
+    for file_name in os.listdir(plugin_folder):
+        if file_name.endswith('.py'):
+            if file_name == "pluglib.py": continue
+            plugin_name = os.path.splitext(file_name)[0]
+            plugins.append(plugin_name)
+    return plugins
+
+def load(plugin_name):
+    plugin_folder = "plugins"
+    plugin_path = os.path.join(plugin_folder, plugin_name + ".py")
+    if not os.path.exists(plugin_path):
+        raise ImportError(f"Plugin '{plugin_name}' not found at '{plugin_path}'")
+    spec = importlib.util.spec_from_file_location(plugin_name, plugin_path)
+    if spec is None:
+        raise ImportError(f"Failed to load plugin '{plugin_name}'")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+for plugin in discover_plugins(plugin_folder()):
+    main_menu.append(load(plugin).main())
+
+main_menu_names = [i.name for i in main_menu]
+
+selected = fzfmenus.menu(main_menu_names)
+for plugin in main_menu:
+    if plugin.name == selected:
+        plugin.selected()
+        break
